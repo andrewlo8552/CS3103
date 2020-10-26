7e8082cb1942a7700a78939a16fa9b238c7ad132
ITokla <61913925+ITokla@users.noreply.github.com>
2020-10-26 16:46:27 +0800

    Add files via upload

---
+++ pzip.c
@@ -0,0 +1,29 @@
+/*
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your group information here.
+ *
+ * Group No.: 10 (Join a project group in Canvas)
+ * First member's full name: Tom Chan
+ * First member's email address: cctom2-c@my.cityu.edu.hk
+ * Second member's full name: (leave blank if none)
+ * Second member's email address: (leave blank if none)
+ * Third member's full name: (leave blank if none)
+ * Third member's email address: (leave blank if none)
+ */
+
+// add/remove header files as you need
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
